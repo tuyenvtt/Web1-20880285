8fdfba65ad14f75aebebba411636c48f189bd2f4
Thanh Tuyen <tuyenvo.lk@gmail.com>
2022-10-30 22:53:20 +0700

    update web

--- js/20880285.js
+++ js/20880285.js
@@ -28,3 +28,61 @@ async function getAuthenticateToken(username, password){
     }
     throw new Error(result.message);
 }
+
+async function login(e){
+    e.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    try{
+        let token = await getAuthenticateToken(username, password);
+        if(token){
+            localStorage.setItem('token', token);
+            document.getElementsByClassName('btn-close')(0).click();
+            displayControls();
+        }
+    }catch(error){
+        document.getElementById('errorMessage').innerHTML = error;
+        displayControls(false);
+    }
+}
+
+function displayControls(isLogin = true){
+    let linkLogins = document.getElementsByClassName('linkLogin');
+    let linkLogouts = document.getElementsByClassName('linkLogout');
+    
+    let displayLogin = 'none';
+    let displayLogout = 'block';
+    if(!isLogin){
+        displayLogin = 'block';
+        displayLogout = 'none';
+    }
+    for(let i =0; i<linkLogins.length; i++){
+        linkLogins[i].style.display = displayLogin;
+        linkLogouts[i].style.display = displayLogout;
+    }
+}
+
+async function checkLogin(){
+    let isLogin = await verifyToken();
+    displayControls(isLogin);
+}
+
+async function verifyToken(){
+    let token = localStorage.getItem('token');
+    if(token){
+        let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'bearer' + token
+            }
+        });
+        if(response.status == 200){
+            return true;
+        }
+    }
+    return false;
+}
